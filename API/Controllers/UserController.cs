a415b08e6ba71e91b299fb03120a1673b6aca8c4
PatelBhargavP <37446238+PatelBhargavP@users.noreply.github.com>
2025-03-17 17:35:56 +0530

    Converteds the users endpoint to Async

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
@@ -9,17 +10,17 @@ namespace API.Controllers;
 public class UserController(DataContext context) : ControllerBase
 {
     [HttpGet]
-    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
-        var users = context.Users.ToList();
+        var users = await context.Users.ToListAsync();
 
         return users;
     }
 
-    [HttpGet("{id}")] //api/user/3
-    public ActionResult<AppUser> GetUsers(int id)
+    [HttpGet("{id:int}")] //api/user/3
+    public async Task<ActionResult<AppUser>> GetUsers(int id)
     {
-        var user = context.Users.FirstOrDefault(u => u.Id == id);
+        var user = await context.Users.FirstOrDefaultAsync(u => u.Id == id);
 
         if (user == null) return NotFound();
 
