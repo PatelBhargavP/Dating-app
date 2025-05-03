847a4f38e6c31cfa48887e0c1360ca4926bd980e
PatelBhargavP <37446238+PatelBhargavP@users.noreply.github.com>
2025-05-03 16:32:54 +0530

    Added global exception middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API;
 using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 app.UseAuthentication();
 app.UseAuthorization();
